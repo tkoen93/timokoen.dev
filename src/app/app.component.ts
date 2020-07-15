98106aacfb0bc74d970ac8c2b41f2ec04cd1d64f
tkoen93 <timokoen93@gmail.com>
2020-07-15 11:14:56 +0200

    Add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { fadeAnimation } from './_animations/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent {
   title = 'timokoendev';
