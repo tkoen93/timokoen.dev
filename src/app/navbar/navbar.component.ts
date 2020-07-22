af6070a12330c4f9902d0ee76e5b4e128a475e62
tkoen93 <timokoen93@gmail.com>
2020-07-22 09:10:06 +0200

    use some angular magic for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faHouseDamage, faAddressCard, faBriefcase, faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { NavBarModel } from '../_models/navbar.model';
 
 @Component({
   selector: 'app-navbar',
@@ -17,13 +18,20 @@ export class NavbarComponent implements OnInit {
 
   selectedRoute: string = "Home";
 
+  navItems: NavBarModel[] = [
+    { icon: faHouseDamage, route: '/', name: 'Home'},
+    { icon: faAddressCard, route: '/about', name: 'About Me'},
+    { icon: faBriefcase, route: '/projects', name: 'Portfolio'},
+    { icon: faIdCardAlt, route: '/contact', name: 'Contact'}
+  ];
+
   constructor(private router: Router,
               private location: Location) {
                 router.events.subscribe(val => {
                   if (location.path() != "") {
                     this.selectedRoute = location.path();
                   } else {
-                    this.selectedRoute = "Home";
+                    this.selectedRoute = "/";
                   }
                 });
               }
