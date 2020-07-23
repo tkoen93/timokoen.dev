f961f617f4f5e99e39a912f87a938e5d4dadb0a5
tkoen93 <timokoen93@gmail.com>
2020-07-23 12:39:41 +0200

    Work in progress July 23rd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FaModel } from '../_models/fa.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  faIcons: FaModel[] = [
+    { icon: faGithub, url: 'https://github.com/tkoen93' },
+    { icon: faLinkedin, url: 'https://www.linkedin.com/in/timokoen/' }
+  ];
 
   constructor() { }
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faHouseDamage, faAddressCard, faBriefcase, faIdCardAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { NavBarModel } from '../_models/navbar.model';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger(
+      'showHideFaIcons',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('600ms ease-out',
+              style({ opacity: 1}))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('600ms ease-in',
+              style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class NavbarComponent implements OnInit {
 
+  @Input() navType: string;
+
   selectedRoute: string = "Home";
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  showNavIcons: boolean = false;
 
   navItems: NavBarModel[] = [
     { icon: faHouseDamage, route: '/', name: 'Home' },
@@ -25,8 +55,10 @@ export class NavbarComponent implements OnInit {
                 router.events.subscribe(val => {
                   if (location.path() != "") {
                     this.selectedRoute = location.path();
+                    this.showNavIcons = true;
                   } else {
                     this.selectedRoute = "/";
+                    this.showNavIcons = false;
                   }
                 });
               }
