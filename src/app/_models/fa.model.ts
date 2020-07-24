7c0b0365f79f894a17fc507d5b08c5add650507e
tkoen93 <timokoen93@gmail.com>
2020-07-24 11:18:06 +0200

    Add const of faIcons

--- src/app/_models/fa.model.ts
+++ src/app/_models/fa.model.ts
@@ -1,4 +1,11 @@
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 export class FaModel {
     icon: any;
     url: string;
 }
+
+export const faIcons: FaModel[] = [
+    { icon: faGithub, url: 'https://github.com/tkoen93' },
+    { icon: faLinkedin, url: 'https://www.linkedin.com/in/timokoen/' }
+  ];
